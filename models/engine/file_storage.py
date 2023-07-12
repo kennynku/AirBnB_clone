b6e34c0c5eb0b78da9baf1af4758df69101d917a
K NKUATSANA <nkuatsanakeneuoe@gmail.com>
2023-07-12 12:19:54 +0200

    file storage implementation
    Co-authored-by: Ntsako Ngoveni <naction923@gmail.com>

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""Implementation of storage"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -11,40 +11,37 @@ from models.state import State
 
 
 class FileStorage:
-    """Represent an abstracted storage engine
-        Attributes:
-            __file_path: string - path to the JSON file (ex: file.json)
-            __objects: dictionary - empty but will store all object
+    """Create a storage engine
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns dictionary object"""
+        """Displays all objects in storage"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Create new object"""
         cname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(cname, obj.id)] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file"""
+        """Serialization"""
         odict = FileStorage.__objects
-        # json takes input as a dict mainly so we convert it to dict
+        # Serialize helper
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as j:
             json.dump(objdict, j)
 
     def reload(self):
-        """deserializes the JSON file to __objects if exists"""
+        """Deserialization"""
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
-                    # after evaluating unpack dict(**)
+                    # Expands the dictionary format
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
