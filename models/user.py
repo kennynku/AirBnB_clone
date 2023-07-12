cde93050ed8294c3b9be95a3029de2bec48922df
K NKUATSANA <nkuatsanakeneuoe@gmail.com>
2023-07-12 12:34:08 +0200

    User class creation
    Co-authored-by: Ntsako Ngoveni <naction923@gmail.com>

--- models/user.py
+++ models/user.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Defines a user module"""
+"""Implementation of user model"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a user
-    Attributes:
-            email(str) - email of user
-            password(str) - password of a user
-            first_name(str) - first name of user
-            last_name(str) - last_name of User
-            """
+    """User class"""
     email = ""
     password = ""
     first_name = ""
